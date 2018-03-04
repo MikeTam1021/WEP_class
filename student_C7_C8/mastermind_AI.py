764ef6b74ffe46b1576aaa732bd8db52b1b0413b
Mike Tamillow <mikaeltamillow96@gmail.com>
2018-03-04 13:49:15 -0600

    mastermind AI changes

--- student_C7_C8/mastermind_AI.py
+++ student_C7_C8/mastermind_AI.py
@@ -1,34 +1,42 @@
 # Mastermind.py
 
 import random
+import itertools
 import time
+import numpy as np
 
-def all_color_list(color_list):
-    all_colors = []
-    for color1 in color_list:
-        for color2 in color_list:
-            for color3 in color_list:
-                for color4 in color_list:
-                    all_colors.append([color1, color2, color3, color4])
-    return all_colors
-
-def one_guess(all_remaining_colors):
-    return random.choice(all_remaining_colors)
-
+INSTRUCTIONS = """\nInstructions:
+                  Guess the right combination of colors in a limited tries,
+                  X means right color, right space.
+                  O means right color, wrong space.
+                  Your color choices are Green, Red,
+                  Blue, White, Yellow, and Purple\n\n"""
 
 def try_all_answers(all_remaining_colors, guess, true_feedback):
-    remaining = []
-    for color_list in all_remaining_colors:
-        selective_feedback = give_feedback(guess, color_list[:])
-        if true_feedback == selective_feedback:
-            remaining.append(color_list)
-    return remaining
+    """
+    This is for the Artificial Intelligence answering mechanism.
 
+    After making a random guess we search all answers that produce the
+    same feedback with the original guess. This results in a smaller set
+    of remaining answers. These answers repeat the random guessing pattern,
+    and take the feedback to produce another, smaller, subset in which
+    the true answer must be contained.
+    """
+
+    remaining = []
+    for maybe_answer in all_remaining_colors:
+        filter_feedback = give_feedback(guess, maybe_answer[:])
+        if true_feedback == filter_feedback:
+            remaining.append(maybe_answer)
+    return remaining
 
 def give_feedback(lguess, answer):
     """
     return X's and O's to give feedback on each guess
+
+    called by the game for the player to respond with a follow-up guess
     """
+
     correct = ''
     idx_list = []
     for i in range(4):
@@ -45,21 +53,24 @@ def give_feedback(lguess, answer):
     return correct
 
 if __name__ == "__main__":
+#number_of_guesses = []
+#failed_attempts = 0
+#for game in range(100):
     tries = 10
+    answer_length = 4
     colors = ['G', 'R', 'B', 'W', 'Y', 'P']
-    answer = [random.choice(colors) for _ in range(4)]
-    all_remaining_colors = all_color_list(colors)
-    print("\nInstructions: Guess the right combination of 4 colors in 10 tries")
-    print("X means right color, right space | O means right color, wrong space")
-    print("Your color choices are Green, Red, Blue, White, Yellow, and Purple\n\n")
+    answer = random.sample(colors, answer_length)
+    all_remaining_colors = list(map(list, itertools.product(colors,
+                                            repeat=answer_length)))
+    print(INSTRUCTIONS)
     while tries > 0:
-        guess = one_guess(all_remaining_colors) #raw_input("Enter 4 colors from G/R/B/W/Y/P, seperate with a space: ")
+        guess = random.choice(all_remaining_colors)
         print("This is the guess for the turn: " + str(guess))
-        #lguess = guess.split(' ')
-        if len(guess) == 4 and set(guess).issubset(set(colors)):
+        if len(guess) == answer_length and set(guess).issubset(set(colors)):
             tries = tries - 1
             if guess == answer:
                 print("you win!!!")
+                #number_of_guesses += [10 - tries]
                 break
             else:
                 feedback = give_feedback(guess, answer[:])
@@ -70,6 +81,15 @@ if __name__ == "__main__":
                 print("answers remaining: " + str(len(all_remaining_colors)))
                 time.sleep(5)
         else:
+            pass
             print("Wrong Format, guess again \n")
+    else:
+        print("You Lost.")
+        #failed_attempts += 1
 
     print("Answer: " + ' '.join(answer))
+
+#all_guesses = np.array(number_of_guesses)
+#print("mean all guess: " + str(np.mean(all_guesses)))
+#print("standard deviation all guess: " + str(np.std(all_guesses)))
+#print("games lost: " + str(failed_attempts))
